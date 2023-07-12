1ac5ce3a0c5ecd90dd702389db1673f63b46e9d8
zlinSG <zlin@monarchtractor.com>
2023-07-13 01:48:57 +0800

    initialize the model if training from scratch

--- models/utils.py
+++ models/utils.py
@@ -2,9 +2,9 @@ import torch
 def init_all(model):
     init_funcs = {
         1: lambda x: torch.nn.init.normal_(x, mean=0.0, std=1.0),  # can be bias
-        2: lambda x: torch.nn.init.xavier_normal_(x, gain=1.0),  # can be weight
-        3: lambda x: torch.nn.init.xavier_uniform_(x, gain=1.0),  # can be conv1D filter
-        4: lambda x: torch.nn.init.xavier_uniform_(x, gain=1.0),  # can be conv2D filter
+        2: lambda x: torch.nn.init.kaiming_uniform_(x, nonlinearity='relu'),  # can be weight
+        3: lambda x: torch.nn.init.kaiming_uniform_(x, nonlinearity='relu'),  # can be conv1D filter
+        4: lambda x: torch.nn.init.kaiming_uniform_(x, nonlinearity='relu'),  # can be conv2D filter
         5: lambda x: torch.nn.init.kaiming_normal_(x, mode="fan_out"),
         "default": lambda x: torch.nn.init.constant(x, 1.0),  # everything else
     }
